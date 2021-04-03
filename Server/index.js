e458780f5ac32cc98c82a30313e179f7355d6251
sbrandon0432 <sbrandon0432@yahoo.com>
2021-04-02 19:09:23 -0700

    added server for html

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.resolve('Client', 'dist')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
