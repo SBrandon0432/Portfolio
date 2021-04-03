d1f79a5edc0ddee30fe44e7ca487782355416062
sbrandon0432 <sbrandon0432@yahoo.com>
2021-04-02 19:08:16 -0700

    import statement fix for css

--- Client/src/components/App/App.tsx
+++ Client/src/components/App/App.tsx
@@ -2,17 +2,57 @@ import React from "react";
 import Resume from "../Resume/Resume";
 import Carousel from '../Carousel/Carousel';
 import Paragraph from '../Paragraph/Paragraph';
+import './AppS.scss';
 
-const App = () => {
-  return (
-  <div className="App">
-    <h1 className="title">Samuel Brandon's Personal Portfolio</h1>
-    <Resume />
-    <h1 className="about"> About Me</h1>
-    <Carousel />
-    <Paragraph />
-    <h1>contact form?</h1>
-  </div>);
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        <h1 className="title" >Samuel's Personal Portfolio</h1>
+
+        <div className="resumeDivInApp">
+          <Resume />
+        </div>
+
+        <h1 className="about"> About Me</h1>
+        <div className="carouselInApp">
+          <Carousel />
+        </div>
+
+        <div className="aboutInApp">
+          <Paragraph />
+        </div>
+
+        <h1>contact form?</h1>
+
+      </div>
+      );
+  }
+  // return (
+  // <div className="App">
+
+  //   <div id="test">Samuel Brandon's Personal Portfolio</div>
+
+  //   <div className="resumeDivInApp">
+  //     <Resume />
+  //   </div>
+
+  //   <h1 className="about"> About Me</h1>
+  //   <div className="carouselInApp">
+  //     <Carousel />
+  //   </div>
+
+  //   <div className="aboutInApp">
+  //     <Paragraph />
+  //   </div>
+
+  //   <h1>contact form?</h1>
+
+  // </div>
+  // );
 }
 
 export default App;
